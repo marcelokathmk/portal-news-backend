070947096288d25e2ea022b903753c6794e8e175
Marcelo Kath <marcelo.kath@4elements-gruppe.de>
2020-03-02 12:24:29 +0100

    Entidades de Categoria e Tags utilizadas nos posts.

--- src/main/java/br/com/portal/portalbackend/model/entity/Post.java
+++ src/main/java/br/com/portal/portalbackend/model/entity/Post.java
@@ -1,11 +1,20 @@
 package br.com.portal.portalbackend.model.entity;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import br.com.portal.portalbackend.model.entity.adm.Categoria;
+import br.com.portal.portalbackend.model.entity.adm.Tag;
 import br.com.portal.portalbackend.model.entity.adm.UsuarioAuditoria;
 
 @Entity
@@ -31,6 +40,32 @@ public class Post extends UsuarioAuditoria {
 	
 	@Column(name = "is_compartilhamento")
 	private Boolean compartilhamentoAtivo = false;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "post_tag", 
+			  	joinColumns = @JoinColumn(name = "id_post"), 
+			  	inverseJoinColumns = @JoinColumn(name = "id_tag")) 
+	private Set<Tag> tags;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_categoria")
+	private Categoria categoria;
+
+	public Set<Tag> getTags() {
+		return tags;
+	}
+
+	public void setTags(Set<Tag> tags) {
+		this.tags = tags;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 
 	public Long getId() {
 		return id;
